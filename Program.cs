0216585c795f7f1e56b2935f63829f3b2711df31
CherryRedDragon <cherryreddragon@gmail.com>
2021-02-26 12:55:02 +0000

    streamlined initial Convert.ToDouble

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a value followed by enter");
-            string firstValue = Console.ReadLine();
-            double firstDouble = Convert.ToDouble(firstValue);
+            //string firstValue = Console.ReadLine();
+            double firstDouble = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Enter a mathematical operation followed by enter");
             string operation = Console.ReadLine();
 
             Console.WriteLine("Enter a second value followed by enter");
-            string secondValue = Console.ReadLine();
-            double secondDouble = Convert.ToDouble(secondValue);
+            //string secondValue = Console.ReadLine();
+            double secondDouble = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Calculating...");
 
